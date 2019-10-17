ddb412a61b7018ba7dc88c6fa5937f1db0d9a3e8
Andrew Stuart <94andrew.stuart@gmail.com>
2019-10-17 16:48:03 +1100

    Changed range from -180 to 180 to 0 - 360

--- src/calc_orient/scripts/calc_orienant_orientation.py
+++ src/calc_orient/scripts/calc_orienant_orientation.py
@@ -25,12 +25,16 @@ class Entity:
         y = math.sin(external_entity.longitude - self.longitude) * math.cos(external_entity.latitude)
         x = math.cos(self.latitude)*math.sin(external_entity.latitude) - math.sin(self.latitude)*math.cos(external_entity.latitude)*math.cos(external_entity.longitude-self.longitude)
         bearing = math.degrees(math.atan2(y, x))
+        if(bearing < 0):
+            bearing = 360 - bearing
         return bearing
 
     def getBearingToEntity2(self, extern_entity_lat, extern_entity_long):
         y = math.sin(extern_entity_long - self.longitude) * math.cos(extern_entity_lat)
         x = math.cos(self.latitude)*math.sin(extern_entity_lat) - math.sin(self.latitude)*math.cos(extern_entity_lat)*math.cos(extern_entity_long-self.longitude)
         bearing = math.degrees(math.atan2(y, x))
+        if(bearing < 0):
+            bearing = 360 - bearing
         return bearing
 
 
