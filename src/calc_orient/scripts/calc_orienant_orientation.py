9e514924f90972c054bf95406f8a1e052d5f3a92
Andrew Stuart <94andrew.stuart@gmail.com>
2019-10-28 18:12:09 +1100

    Sweep mode

--- src/calc_orient/scripts/calc_orienant_orientation.py
+++ src/calc_orient/scripts/calc_orienant_orientation.py
@@ -15,6 +15,7 @@ radio_signal_strength = -96
 orient_pub = rospy.Publisher('ant_orientation', Point32, queue_size=10)
 msg = Point32()
 sweeping_enabled = False
+sweep_increment_array = [5.0, -5.0, 10.0, -10.0, 15.0, -15.0, 20.0, -20.0, 25.0, -25.0, 30.0, -30.0, 35.0, -35.0, 40.0, -40.0, 45.0, -45.0, 50.0, -50.0, 55.0, -55.0, 60.0, -60.0, 65.0, -65.0, 70.0, -70.0, 75.0, -75.0, 80.0, -80.0, 85.0, -85.0, 90.0, -90.0]
 
 class Entity:
     def __init__(self, name = 'Untitled'):
@@ -109,6 +110,96 @@ def sweepCallback(data):
     global antenna_is_auto
     global radio_tx_rate
     sweeping_enabled = data
+    if(sweeping_enabled):
+        sweep_azimuth_origin = antenna.azimuth
+        sweep_elevation_origin = antenna.elevation
+        msg.x = sweep_azimuth_origin
+        msg.y = sweep_elevation_origin
+        orient_pub.publish(msg)
+        time.sleep(1)
+        sweep_count = 0
+        previous_position = sweep_azimuth_origin
+        sweep_direction_clockwise = True
+        crossover = False
+        while(sweeping_enabled and antenna_is_auto):
+            current_azimuth_position = antenna.azimuth
+            current_elevation_position = antenna.elevation
+            next_sweep_azimuth_position = sweep_azimuth_origin + sweep_increment_array[sweep_count]
+            if(next_sweep_azimuth_position < 0):
+                next_sweep_azimuth_position = 360.0 + (next_sweep_azimuth_position)
+            elif (next_sweep_azimuth_position >= 360.0):
+                next_sweep_azimuth_position = next_sweep_azimuth_position - 360.0
+            next_sweep_elevation_position = sweep_elevation_origin
+            msg.x = next_sweep_azimuth_position
+            msg.y = next_sweep_elevation_position
+            orient_pub.publish(msg)
+            radio_disconnected = True
+            if(sweep_direction_clockwise):
+                if(next_sweep_azimuth_position < current_azimuth_position):
+                    crossover = True
+                else:
+                    crossover =  False
+            else:
+                if(current_azimuth_position < next_sweep_azimuth_position):
+                    crossover = True
+                else:
+                    crossover = False
+            limit_not_reached = True
+            while(radio_disconnected and limit_not_reached):
+                time.sleep(0.5)
+                if(radio_tx_rate > 0):
+                    radio_disconnected = False
+                    sweeping_enabled = False
+                    rospy.loginfo("RADIO CONNECTION RE-ESTABLISHED")
+                    msg.x = antenna.azimuth
+                    msg.y = antenna.elevation
+                    orient_pub.publish(msg)
+                else:
+                    if(crossover):
+                        if(sweep_direction_clockwise):
+                            if(antenna.azimuth > next_sweep_azimuth_position):
+                                pass
+                            else:
+                                if(antenna.azimuth >= next_sweep_azimuth_position):
+                                    sweep_direction_clockwise = False
+                                    limit_not_reached = False
+                                    sweep_count = sweep_count + 1
+                                    break
+                                else:
+                                    pass
+                        else:
+                            if(antenna.azimuth < next_sweep_azimuth_position):
+                                pass
+                            else:
+                                if(antenna.azimuth <= next_sweep_azimuth_position):
+                                    sweep_direction_clockwise = True
+                                    limit_not_reached = False
+                                    sweep_count = sweep_count + 1
+                                    break
+                                else:
+                                    pass
+                    else:
+                        if(sweep_direction_clockwise):
+                            if(antenna.azimuth >= next_sweep_azimuth_position):
+                                    sweep_direction_clockwise = False
+                                    limit_not_reached = False
+                                    sweep_count = sweep_count + 1
+                                    break
+                            else:
+                                pass
+                        else:
+                            if(antenna.azimuth <= next_sweep_azimuth_position):
+                                    sweep_direction_clockwise = True
+                                    limit_not_reached = False
+                                    sweep_count = sweep_count + 1
+                                    break
+                            else:
+                                pass
+
+
+
+                
+
 def radioTXCallback(data):
     global radio_tx_rate
     radio_tx_rate = data.data
@@ -124,6 +215,7 @@ def radioSignalCallback(data):
 def currentOrientationCallback(data):
     antenna.azimuth = data.x
     antenna.elevation = data.y
+
 def calculateAntennaOrientation():
     rospy.Subscriber('ant_gps', NavSatFix, antGPSCallback)
     rospy.Subscriber('rover_gps', NavSatFix, roverGPSCallback)
